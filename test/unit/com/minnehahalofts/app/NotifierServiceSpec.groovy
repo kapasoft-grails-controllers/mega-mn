beda1a453a89f8b4f048fcd14383e049e0f33f4f
Margots Kapacs <makapacs@hotmail.com>
2013-07-02 14:10:23 -0500

    first commit

---
+++ test/unit/com/minnehahalofts/app/NotifierServiceSpec.groovy
@@ -0,0 +1,17 @@
+package com.minnehahalofts.app
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(NotifierService)
+class NotifierServiceSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
